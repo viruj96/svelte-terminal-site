26fa0f660b87599df9659d764b037a8dd7663e3a
Viruj <virujbala.96@gmail.com>
2022-10-03 19:29:43 +0400

    key press sounds

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,20 @@ export const dateTimeFormatter = new Intl.DateTimeFormat("en", {
 	second: "2-digit",
 	weekday: "long",
 });
+export const playSound = (key) => {
+	const dirPath = 'src/assets/sounds/';
+	let sound;
+	if (key === 'Enter') {
+		sound = dirPath + 'enter.wav';
+	} else if (key === 'Backspace') {
+		sound = dirPath + 'backspace.wav';
+	} else {
+		const sounds = [];
+		for (let i = 0; i < 3; i++) {
+			sounds.push(`${dirPath}key${i + 1}.mp3`);
+		}
+		sound = sounds[Math.floor(Math.random() * sounds.length)];
+	}
+	const audio = new Audio(sound);
+	audio.play();
+};
